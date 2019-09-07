d6895c88786fe4e7a626dd13d93c032423de3f7f
badCompany55 <zachirvin45@gmail.com>
2019-09-07 14:19:10 -0500

    Refactor queue to us DLL

--- queue/queue.py
+++ queue/queue.py
@@ -1,29 +1,53 @@
+import sys
+sys.path.append('../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
+# class Queue:
+#   def __init__(self):
+#       self.size = 0
+#     # what data structure should we
+#     # use to store queue elements?
+#       self.storage = []
+#
+#   def enqueue(self, item):
+#       self.storage.insert(0, item)
+#       self.size = len(self.storage)
+#   
+#   def dequeue(self):
+#       if self.size > 0:
+#           self.size = self.size - 1
+#           return self.storage.pop()
+#       else:
+#           pass
+#
+#   def len(self):
+#       if self.size == 0:
+#           return 0 
+#       else:
+#           return self.size
+
+
+d_link_list = DoublyLinkedList()
+
 class Queue:
-  def __init__(self):
-      self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-      self.storage = []
+    def __init__(self):
+        self.size = 0
+        self.storage = d_link_list
 
-  def enqueue(self, item):
-      self.storage.insert(0, item)
-      self.size = len(self.storage)
-  
-  def dequeue(self):
-      if self.size > 0:
-          self.size = self.size - 1
-          return self.storage.pop()
-      else:
-          pass
+    def enqueue(self, item):
+        self.size += 1
+        self.storage.add_to_tail(item)
 
-  def len(self):
-      if self.size == 0:
-          return 0 
-      else:
-          return self.size
+    def dequeue(self):
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_from_head()
+        else:
+            return
+
+    def len(self):
+        return self.size
 
 
-q = Queue()
-q.enqueue(100)
+test = Queue()
 
-print(q.storage)
+
