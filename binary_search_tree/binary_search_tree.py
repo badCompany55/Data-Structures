3bf0e00458971d19cea7dac8a9081aea1cd67da8
badCompany55 <zachirvin45@gmail.com>
2019-09-10 21:28:58 -0500

    BST COMPLETE

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,13 +5,47 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+      node = BinarySearchTree(value)
+      if value < self.value:
+          if self.left:
+              self.left.insert(value)
+          else:
+              self.left = node
+      else:
+          if self.right:
+              self.right.insert(value)
+          else:
+              self.right = node
 
   def contains(self, target):
-    pass
+      if self.value == target:
+          return True
+      if target < self.value:
+          if self.left:
+              return self.left.contains(target)
+          else:
+              return False
+      else:
+          if self.right:
+              return self.right.contains(target)
+          else:
+              return False
 
   def get_max(self):
-    pass
+      if self.right is None:
+          return self.value
+      if self.right:
+          return self.right.get_max()
 
   def for_each(self, cb):
-    pass
+      cb(self.value)
+      if self.right:
+          self.right.for_each(cb)
+      if self.left:
+          self.left.for_each(cb)
+
+
+bst = BinarySearchTree(0)
+bst.insert(7)
+bst.insert(5)
+print(bst.contains(7))
