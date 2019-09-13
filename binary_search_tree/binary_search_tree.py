3e82ef1d9115867db3e2049fa990d05c429141f0
badCompany55 <zachirvin45@gmail.com>
2019-09-13 00:17:50 -0500

    complete MVP Heap max complete

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -37,12 +37,44 @@ class BinarySearchTree:
       if self.right:
           return self.right.get_max()
 
+# depth first traversal
+# =======================================================
   def for_each(self, cb):
       cb(self.value)
       if self.right:
           self.right.for_each(cb)
       if self.left:
           self.left.for_each(cb)
+
+# depth first traversal
+# =======================================================
+  def dft_non_recursive(self, cb):
+      stack = []
+      stack.append(self)
+
+      while stack:
+          current_node = stack.pop()
+          if current_node.left:
+              stack.append(current_node.left)
+          if current_node.right:
+              stack.append(current_node.right)
+
+          cb(current_node.value)
+
+# depth first traversal "can't be recursive"
+# =======================================================
+def bft_for_each(self, cb):
+    q = Queue()
+    q.enqueue(self)
+    while(q):
+        current_node = q.dequeue()
+        if current_node.left:
+            q.enqueue(current_node.left)
+
+        if current_node.right:
+            q.enqueue(current_node.right)
+
+        cb(current_node.value)
 
 
 bst = BinarySearchTree(0)
